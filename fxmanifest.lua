3d78e5b04fdb17a0d0c3a6c337a56463a19b1e63
Kuba200 Devplace <76246649+XKuba200X@users.noreply.github.com>
2022-06-02 20:10:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+	'Config.lua',
+	'Client/*.lua'
+}
+
+server_scripts {
+	'Config.lua',
+	'@oxmysql/lib/MySQL.lua',
+	'Server/*.lua'
+}
+
+exports {
+	'OpenMainAnimationsMenu',
+	'GetEmotes',
+	'RequestLatestStyle',
+	'EmoteStart'
+}
